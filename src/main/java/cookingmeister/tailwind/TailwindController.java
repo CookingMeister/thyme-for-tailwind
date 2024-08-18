00539aa7da29b164964836891badf6b38d056722
Shawn Meister <shawnmeister@Elizabeths-MacBook-Air.local>
2024-08-18 10:21:50 -0300

    add tailwind css to thymeleaf app using node and npm to inject main.css into static folder

---
+++ src/main/java/cookingmeister/tailwind/TailwindController.java
@@ -0,0 +1,10 @@
+package cookingmeister.tailwind;
+
+import org.springframework.stereotype.Controller;
+
+@Controller
+public class TailwindController {
+    public String index() {
+        return "index";
+    }
+}
